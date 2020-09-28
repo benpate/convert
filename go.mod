2acf4e5386d68c242cca06fa49031f1452b47501
Ben Pate <ben@pate.org>
2020-09-27 20:20:38 -0600

    Test updates

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/benpate/convert
 go 1.14
 
 require (
-	github.com/stretchr/testify v1.6.1
 	github.com/benpate/null v0.5.0
+	github.com/stretchr/testify v1.6.1
 )
--- int_test.go
+++ int_test.go
@@ -3,36 +3,36 @@ package convert
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 // TODO: Thes tests still dont account for overflow errors.
 
 func TestInt(t *testing.T) {
-	assert.Equal(t, IntDefault(10, 10), int(10))
+	require.Equal(t, IntDefault(10, 10), int(10))
 }
 
 func TestNilToInt(t *testing.T) {
 
 	result, natural := IntOk(nil, int(-1))
-	assert.Equal(t, result, int(-1))
-	assert.False(t, natural)
+	require.Equal(t, result, int(-1))
+	require.False(t, natural)
 }
 
 func TestFloat32ToInt(t *testing.T) {
 
 	result, natural := IntOk(float32(10), -1)
 
-	assert.False(t, natural)
-	assert.Equal(t, result, int(10))
+	require.False(t, natural)
+	require.Equal(t, result, int(10))
 }
 
 func TestFloat64ToInt(t *testing.T) {
 
 	result, natural := IntOk(float64(10), -1)
 
-	assert.False(t, natural)
-	assert.Equal(t, result, int(10))
+	require.False(t, natural)
+	require.Equal(t, result, int(10))
 }
 
 func TestIntToInt(t *testing.T) {
@@ -40,54 +40,61 @@ func TestIntToInt(t *testing.T) {
 	{
 		result, natural := IntOk(int(10), -1)
 
-		assert.True(t, natural)
-		assert.Equal(t, result, int(10))
+		require.True(t, natural)
+		require.Equal(t, result, int(10))
 	}
 
 	{
 		result, natural := IntOk(int8(10), -1)
 
-		assert.True(t, natural)
-		assert.Equal(t, result, int(10))
+		require.True(t, natural)
+		require.Equal(t, result, int(10))
 	}
 
 	{
 		result, natural := IntOk(int16(10), -1)
 
-		assert.True(t, natural)
-		assert.Equal(t, result, int(10))
+		require.True(t, natural)
+		require.Equal(t, result, int(10))
 	}
 
 	{
 		result, natural := IntOk(int32(10), -1)
 
-		assert.True(t, natural)
-		assert.Equal(t, result, int(10))
+		require.True(t, natural)
+		require.Equal(t, result, int(10))
 	}
 
 	{
 		result, natural := IntOk(int64(10), -1)
 
-		assert.True(t, natural)
-		assert.Equal(t, result, int(10))
+		require.True(t, natural)
+		require.Equal(t, result, int(10))
 	}
 }
 
 func TestStringToInt(t *testing.T) {
 
 	{
+		result, ok := IntOk("0", -1)
+		require.Zero(t, result)
+		require.False(t, ok)
+	}
+
+	{
 		result, natural := IntOk("10", -1)
 
-		assert.False(t, natural)
-		assert.Equal(t, result, int(10))
+		require.False(t, natural)
+		require.Equal(t, result, int(10))
 	}
 
 	{
 		result, natural := IntOk("invalid", -1)
 
-		assert.False(t, natural)
-		assert.Equal(t, result, int(-1))
+		require.False(t, natural)
+		require.Equal(t, result, int(-1))
 	}
+
 }
 
 func TestStringerToInt(t *testing.T) {
@@ -97,22 +104,22 @@ func TestStringerToInt(t *testing.T) {
 	{
 		result, natural := IntOk(s, -1)
 
-		assert.False(t, natural)
-		assert.Equal(t, result, int(-1))
+		require.False(t, natural)
+		require.Equal(t, result, int(-1))
 	}
 
 	s[0] = "100"
 	{
 		result, natural := IntOk(s, -1)
 
-		assert.False(t, natural)
-		assert.Equal(t, result, int(100))
+		require.False(t, natural)
+		require.Equal(t, result, int(100))
 	}
 }
 
 func TestInvalidToInt(t *testing.T) {
 	result, natural := IntOk(map[string]interface{}{}, -1)
 
-	assert.False(t, natural)
-	assert.Equal(t, result, int(-1))
+	require.False(t, natural)
+	require.Equal(t, result, int(-1))
 }
